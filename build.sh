010e88505d2d3c2372ddcd4b73b1f0377d11a740
antagomir <lei@MIB12001>
2013-02-18 15:32:50 +0100

    Build checks OK

--- build.sh
+++ build.sh
@@ -2,5 +2,3 @@
 ~/bin/R-2.15.2/bin/R CMD check earlywarnings
 ~/bin/R-2.15.2/bin/R CMD build earlywarnings
 ~/bin/R-2.15.2/bin/R CMD INSTALL earlywarnings_1.0.32.tar.gz
-
-
---
+++ installation.script.R
@@ -0,0 +1,22 @@
+# R Installation script for earlywarnings package.
+# Install dependencies, then the package.
+
+# Needed for earlywarnings
+install.packages("moments")
+install.packages("quadprog")
+install.packages("nortest")
+install.packages("Kendall")
+install.packages("som")
+install.packages("akima")
+install.packages("tseries")
+install.packages("splus2R")
+install.packages("ifultools")
+install.packages("sapa")
+install.packages("wmtsa")
+install.packages("scatterplot3d")
+system("wget http://cran.r-project.org/src/contrib/Archive/fractal/fractal_1.1-1.tar.gz")
+install.packages("fractal_1.1-1.tar.gz", repos = NULL)
+
+#library(devtools); install_github(repo = "earlywarnings-R", username = "earlywarningtoolbox")
+#install_github(repo = "earlywarnings-R", username = "earlywarningtoolbox", type = "source", ref = "develop")
+
