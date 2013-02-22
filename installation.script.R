c61a728644c8bea00159706e5e362c7e5b96f801
vdakos <vasilis.dakos@gmail.com>
2013-02-22 22:50:13 +0100

    Update installation.script.R

--- installation.script.R
+++ installation.script.R
@@ -17,6 +17,7 @@ install.packages("scatterplot3d")
 #system("wget http://cran.r-project.org/src/contrib/Archive/fractal/fractal_1.1-1.tar.gz")
 #install.packages("fractal_1.1-1.tar.gz", repos = NULL)
 
-library(devtools); install_github(repo = "earlywarnings-R", username = "earlywarningtoolbox", subdir = "earlywarnings", ref ="master")
+library(devtools)
+install_github(repo = "earlywarnings-R", username = "earlywarningtoolbox", subdir = "earlywarnings", ref ="master")
 #install_github(repo = "earlywarnings-R", username = "earlywarningtoolbox", type = "source", ref = "develop")
 
