4a3eddf4c63511307733c9c8572b8c8732ccd708
antagomir <antagomir@opencomp>
2013-03-12 16:15:22 +0100

    syncing

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ~/bin/R-2.15.2/bin/R CMD BATCH document.R
 ~/bin/R-2.15.2/bin/R CMD check earlywarnings
 ~/bin/R-2.15.2/bin/R CMD build earlywarnings
-~/bin/R-2.15.2/bin/R CMD INSTALL earlywarnings_1.0.33.tar.gz
+~/bin/R-2.15.2/bin/R CMD INSTALL earlywarnings_1.0.34.tar.gz
