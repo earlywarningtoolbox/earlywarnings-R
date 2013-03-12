a04e11c008a2178bee94828e4d483bf96207f8b2
vasilis dakos <vasilis.dakos@gmail.com>
2013-03-12 15:28:59 +0100

    updated Rpackage, included RShiny

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-~/bin/R-2.15.2/bin/R CMD BATCH document.R
-~/bin/R-2.15.2/bin/R CMD check earlywarnings
-~/bin/R-2.15.2/bin/R CMD build earlywarnings
-~/bin/R-2.15.2/bin/R CMD INSTALL earlywarnings_1.0.33.tar.gz
+R CMD BATCH document.R
+R CMD check earlywarnings
+R CMD build earlywarnings
+R CMD INSTALL earlywarnings_1.0.34.tar.gz
+
+#~/bin/R-2.15.2/bin/R CMD BATCH document.R
+#~/bin/R-2.15.2/bin/R CMD check earlywarnings
+#~/bin/R-2.15.2/bin/R CMD build earlywarnings
+#~/bin/R-2.15.2/bin/R CMD INSTALL earlywarnings_1.0.33.tar.gz
