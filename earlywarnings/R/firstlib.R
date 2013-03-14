e76af7686cc159cf1b9a977eaf47127e440bdcde
vasilis dakos <vasilis.dakos@gmail.com>
2013-03-14 22:55:08 +0100

    update earlywarnings

--- earlywarnings/R/firstlib.R
+++ earlywarnings/R/firstlib.R
@@ -1,4 +1,4 @@
-.onLoad <- function(lib, pkg)
+.onAttach <- function(lib, pkg)
 {
    packageStartupMessage("\nearlywarnings Copyright (C) 2011-2013 Vasilis Dakos et al.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under the FreeBSD open source license. For more information, see http://www.early-warning-signals.org\n")
 
