4df7b87bf61c4ba84bf479773322f6af069c67f9
antagomir <lei@MIB12001>
2013-03-26 15:53:45 +0100

    Added utils.R

---
+++ earlywarnings/R/utils.R
@@ -0,0 +1,27 @@
+
+#' Description: Get group assigment indices for univariate data points, given cluster break points
+#'
+#'
+#'  Arguments:
+#'    @param x Univariate data vector
+#'    @param breakpoints Cluster breakpoints
+#'
+#' Returns:
+#'   @return A vector of cluster indices
+#'
+#' @export
+#'
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @examples
+#'
+#' @keywords early-warning
+
+
+UnivariateGrouping <- function (x, breakpoints) {
+  g <- rep.int(NA, length(x))
+  mps <- c(breakpoints, Inf)
+  for (i in 1:length(mps)) {
+    g[x <= mps[[i]] & is.na(g)] <- i  
+  }
+  g
+}
