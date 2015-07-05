b5b98d2bc0bb5a04f48a0dd2b796908a185bab7f
antagomir <leo.lahti@iki.fi>
2015-07-06 00:50:49 +0200

    ok

--- earlywarnings/inst/extras/build.cran.sh
+++ earlywarnings/inst/extras/build.cran.sh
@@ -1,6 +1,6 @@
 /usr/local/bin/R CMD BATCH document.R
 /usr/local/bin/R CMD build ../../
-/usr/local/bin/R CMD check --as-cran earlywarnings_1.1.21.tar.gz
-/usr/local/bin/R CMD INSTALL earlywarnings_1.1.21.tar.gz
+/usr/local/bin/R CMD check --as-cran earlywarnings_1.1.22.tar.gz
+/usr/local/bin/R CMD INSTALL earlywarnings_1.1.22.tar.gz
 
 
